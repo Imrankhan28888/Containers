338865d774af7a2a4e8d4cb530e4d42e1fad3cf8
Imran <imran_khan2888@hotmail.com>
2024-09-11 13:33:02 -0500

    modify frontend message sep112024wednesday

--- kub-network-07-finished-Minikube/kub-network-07-finished-Minikube/frontend/src/App.js
+++ kub-network-07-finished-Minikube/kub-network-07-finished-Minikube/frontend/src/App.js
@@ -52,7 +52,7 @@ function App() {
         <NewTask onAddTask={addTaskHandler} />
       </section>
       <section>
-        <button onClick={fetchTasks}>Fetch ALL Tasks Sep112024-Wed</button>
+        <button onClick={fetchTasks}>Fetch ALL Tasks Sep112024-Wednesday</button>
         <TaskList tasks={tasks} />
       </section>
     </div>
