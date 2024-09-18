8fea7fee5cd4d498b43f84c7d65d79c9730b0809
Imran <imran_khan2888@hotmail.com>
2024-09-18 14:17:03 -0500

    prod - commit sep18 2:16 pm

--- kub-network-07-finished-Minikube/kub-network-07-finished-Minikube/frontend/src/App.js
+++ kub-network-07-finished-Minikube/kub-network-07-finished-Minikube/frontend/src/App.js
@@ -52,7 +52,7 @@ function App() {
         <NewTask onAddTask={addTaskHandler} />
       </section>
       <section>
-        <button onClick={fetchTasks}>Fetch Tasks Sep18,2024-1:36pm</button>
+        <button onClick={fetchTasks}>Prod - Fetch Tasks Sep18,2024-2:16pm</button>
         <TaskList tasks={tasks} />
       </section> 
     </div>
